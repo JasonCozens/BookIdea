de6d5b92b08ab8161db98f5ac14323a1bd251d05
Edawrd Jason Cozens <e.jason.cozens@gmail.com>
2016-03-09 14:51:31 +0000

    RED: At this point the test is nearly correct but we need to look at controlling the time.

--- TimeTracker/tt/tracker.py
+++ TimeTracker/tt/tracker.py
@@ -1,13 +1,19 @@
+from datetime import datetime
+
 class Tracker:
 
-    def __init__(self):
-        pass
+    def __init__(self, now=datetime.now):
+        self._now = now
+        self._history = []
 
     @property
     def history(self):
-        return ""
+        h = ""
+        for task in self._history:
+            h += "(" + task.strftime("%Y/%m/%d - %H:%M:%S") + "]"
+        return h
 
     def start(self):
-        pass
+        self._history.append(self._now())
 
 # datetime.datetime.strftime(now(), "%Y/%m/%d - %H:%M:%S")
--- TimeTracker/tt/tt_tests/tracker_tests.py
+++ TimeTracker/tt/tt_tests/tracker_tests.py
@@ -11,4 +11,4 @@ class TrackerTest(unittest.TestCase):
     def test_start_time_is_now(self):
         tracker = Tracker()
         tracker.start()
-        self.assertEqual(tracker.history, "(2016/03/09 - 14:38:47]")
+        self.assertEqual("(2016/03/09 - 14:38:47]", tracker.history)
