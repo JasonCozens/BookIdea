fa2d1b94ee13eefa96709e3e01f2e81dbdf6afe6
Edawrd Jason Cozens <e.jason.cozens@gmail.com>
2016-03-09 17:00:11 +0000

    RED: Added failing test for stop.

--- TimeTracker/tt/tt_tests/tracker_tests.py
+++ TimeTracker/tt/tt_tests/tracker_tests.py
@@ -20,7 +20,17 @@ class TrackerTest(unittest.TestCase):
 
     def test_start_time_is_now(self):
         tracker = Tracker(NowMock([
-            datetime(2016, 3, 9, 14, 38, 47)
+            datetime(2016, 3, 9, 14, 38, 47),
         ]).now)
         tracker.start()
         self.assertEqual("(2016-03-09 14:38:47]", tracker.history)
+
+    def test_start_stop(self):
+        tracker = Tracker(NowMock([
+            datetime(2016, 3, 9, 14, 38, 47),
+            datetime(2016, 3, 9, 15, 50, 12),
+        ]).now)
+        tracker.start()
+        self.assertEqual(
+            "(2016-03-09 14:38:47,2016-03-09 15:50:12)",
+            tracker.history)
