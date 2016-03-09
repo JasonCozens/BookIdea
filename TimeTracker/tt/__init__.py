5d3399342c2ab186802b792be15b6ceb5c50db1f
Edawrd Jason Cozens <e.jason.cozens@gmail.com>
2016-03-09 14:15:20 +0000

    Directory set up for project.

---
+++ TimeTracker/tt/__init__.py
@@ -0,0 +1,7 @@
+""" Time Tracker is a study in TDD in python.
+
+Create a time tracker that has two inputs start and stop.
+When start is pressed the time of this is recorded.
+When stop is pressed the time of this is recorded.
+There is a query that lists the duration
+__author__ = 'Jason Cozens'
