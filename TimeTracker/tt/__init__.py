78f62c8306b1d796b3387714ac10f2e925125d84
Edawrd Jason Cozens <e.jason.cozens@gmail.com>
2016-03-09 14:27:33 +0000

    First GREEN, some thoughts on class structure as far as syntactic layout goes.

--- TimeTracker/tt/__init__.py
+++ TimeTracker/tt/__init__.py
@@ -3,5 +3,13 @@
 Create a time tracker that has two inputs start and stop.
 When start is pressed the time of this is recorded.
 When stop is pressed the time of this is recorded.
-There is a query that lists the duration
+There is a query that lists the duration.
+
+How should a class be laid out?
+
+    Properties - This tells the user about the state.
+
+    Queries - This tells more about the state.
+
+    Commands - These change the state.
 """
--- TimeTracker/tt/tracker.py
+++ TimeTracker/tt/tracker.py
@@ -5,4 +5,4 @@ class Tracker:
 
     @property
     def history(self):
-        return None
+        return ""
